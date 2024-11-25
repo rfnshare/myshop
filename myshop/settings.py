432e29bc809434d02effd03710733c10d90283a3
Abdullah Al Faroque <rfnshare@gmail.com>
2024-11-25 19:15:06 +0600

    fixed img size

--- myshop/settings.py
+++ myshop/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'corsheaders',
 ]
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=5),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
